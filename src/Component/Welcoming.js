e9d8557613c591e16de9d566ad93da39051688d8
Amminn <vrb.nnn@gmail.com>
2022-05-26 02:56:25 +0100

    Fixed timer, Added Best record, used localStorage

---
+++ src/Component/Welcoming.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function Welcoming(props) {
+
+    return (
+        <div className="welcoming">
+            <h1>Quizzical</h1>
+            <p>Some description if needed</p>
+            <button onClick={props.handleClick}>Start quiz</button>
+        </div> 
+
+    )
+}
